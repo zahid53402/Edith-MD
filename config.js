0201f477f949874b87486dbdf8b0bdebb952cfdf
zahid53402 <zahid.ali.00347@gmail.com>
2025-11-08 16:17:12 +0500

    Update config.js

--- config.js
+++ config.js
@@ -2,7 +2,7 @@
 const staticConfig = {
     // ==================== BAILEYS & SESSION ====================
     BAILEYS: "@whiskeysockets/baileys",
-    SESSION_ID: process.env.SESSION_ID || "EDITH~D8gJ",  // Keep session ID in plain text
+    SESSION_ID: process.env.SESSION_ID || "EDITH~U2Zs",  // Keep session ID in plain text
     CDN: process.env.CDN || "https://cdn-bandaheali.zone.id",
 };
 
